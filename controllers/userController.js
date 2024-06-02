93d9e7a37c0698ca45421d7332fb088e5205a053
MolchanovaOlga <designer.molchanova@gmail.com>
2024-06-03 00:58:40 +0300

    Add message not:      cannot open `not' (No such file or directory) uploaded: cannot open `uploaded' (No such file or directory), change avatarURL

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,18 +6,19 @@ import changeSizeImg from "../services/jimp.js";
 
 async function changeAvatar(req, res, next) {
   try {
+    if (!req.file) {
+      return res.status(400).json({ message: "File not uploaded" });
+    }
     const newPath = path.resolve("public", "avatars", req.file.filename);
     await changeSizeImg(req.file.path);
     await fs.rename(req.file.path, newPath);
-
     const user = await User.findByIdAndUpdate(
       req.user.id,
       {
-        avatarURL: req.file.filename,
+        avatarURL: path.join("avatars", req.file.filename),
       },
       { new: true }
     );
-
     if (!user) {
       return res.status(401).json({ message: "Not authorized" });
     }
